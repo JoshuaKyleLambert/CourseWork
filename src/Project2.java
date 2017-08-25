70f949f31b40e274449e2f3c72d8fb4dd3a7ef41
Joshua <jl6356@stu.armstrong.edu>
2017-08-25 00:04:49 -0400

    Rejected commit: Default

---
+++ src/Project2.java
@@ -0,0 +1,77 @@
+/**
+ * Created by Joshua Lambert on 8/24/2017.
+ *
+ * Implementation of Merge & Insertion sort Classic Algorithms as found in
+ * the Corman Text.
+ *
+ * This program will also output runtime for performance testing.
+ *
+ */
+public class Project2 {
+
+    public static void mergeSort(int[] a, int p, int q , int r){
+
+
+    }
+
+    public static void mergeSort(int[] a, int p, int r){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void mergeSort(int[] list) {
+        if (list.length > 1) {
+            // Merge sort the first half
+            int[] firstHalf = new int[list.length / 2];
+            System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
+            mergeSort(firstHalf);
+
+            // Merge sort the second half
+            int secondHalfLength = list.length - list.length / 2;
+            int[] secondHalf = new int[secondHalfLength];
+            System.arraycopy(list, list.length / 2,
+                    secondHalf, 0, secondHalfLength);
+            mergeSort(secondHalf);
+
+            // Merge firstHalf with secondHalf into list
+            merge(firstHalf, secondHalf, list);
+        }
+    }
+
+    /**
+     * Merge two sorted lists
+     */
+    public static void merge(int[] list1, int[] list2, int[] temp) {
+        int current1 = 0; // Current index in list1
+        int current2 = 0; // Current index in list2
+        int current3 = 0; // Current index in temp
+
+        while (current1 < list1.length && current2 < list2.length) {
+            if (list1[current1] < list2[current2]) {
+                temp[current3++] = list1[current1++];
+            } else {
+                temp[current3++] = list2[current2++];
+            }
+        }
+
+        while (current1 < list1.length) {
+            temp[current3++] = list1[current1++];
+        }
+
+        while (current2 < list2.length) {
+            temp[current3++] = list2[current2++];
+        }
+    }
+
+}
