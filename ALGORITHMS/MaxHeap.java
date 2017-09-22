fa398c269d087b9b8587bbb40132fed39752bcbb
Joshua <jl6356@stu.armstrong.edu>
2017-09-22 02:03:26 -0400

    cleanup a little

--- ALGORITHMS/MaxHeap.java
+++ ALGORITHMS/MaxHeap.java
@@ -6,14 +6,14 @@ public class MaxHeap {
     // reference to the array
     public int[] a;
     //swap space
-    int swap;
+    private int swap;
 
     // heapSize may be different from a.length
-    public int heapSize;
+    private int heapSize;
 
     // construct a MaxHeap object
     // reference (do not copy) the user supplied array
-    public MaxHeap(int[] a){
+    public MaxHeap(int[] a) {
         this.a = a;
         heapSize = a.length - 1;
         buildHeap();
@@ -26,31 +26,33 @@ public class MaxHeap {
         int l = left(i);
         int r = right(i);
 
-        if (l <= heapSize  && a[l] > a[i]){ largest = l;}
-        else largest = i;
-        if (r <= heapSize  && a[r] > a[largest]){ largest = r;}
-        //System.out.println(heapSize);
+        if (l <= heapSize && a[l] > a[i]) {
+            largest = l;
+        } else largest = i;
+        if (r <= heapSize && a[r] > a[largest]) {
+            largest = r;
+        }
 
         if (largest != i) {
             swap = a[largest];
             a[largest] = a[i];
             a[i] = swap;
-            //--heapSize;
             heapify(largest);
         }
 
     }
 
-    public void buildHeap(){
+    public void buildHeap() {
         heapSize = a.length - 1;
-        for (int i = (a.length - 1 )/2; i >= 0; i--){
+        for (int i = (a.length - 1) / 2; i >= 0; i--) {
             heapify(i);
         }
     }
+
     // heapsort on a
-    public void sort(){
+    public void sort() {
         //buildHeap();
-        for(int i = a.length - 1; i >= 1; i--){
+        for (int i = a.length - 1; i >= 1; i--) {
             swap = a[i];
             a[i] = a[0];
             heapSize = heapSize - 1;
@@ -59,12 +61,13 @@ public class MaxHeap {
     }
 
     // priority queue operations
-    public int maximum(){
+    public int maximum() {
         return a[0];
     }
-    public int extractMax(){
+
+    public int extractMax() {
         int max;
-        if ( heapSize < 0){
+        if (heapSize < 0) {
             System.out.print("Heap underflow");
         }
         max = a[0];
@@ -74,33 +77,35 @@ public class MaxHeap {
         return max;
     }
 
-    public void increaseKey(int i, int key){
-        if ( key < a[i])
+    public void increaseKey(int i, int key) {
+        if (key < a[i])
             System.out.print("new Key is smaller than current key");
         a[i] = key;
-        while ( i > 0 && a[parent(i)] < a[i]){
+        while (i > 0 && a[parent(i)] < a[i]) {
             swap = a[parent(i)];
             a[parent(i)] = a[i];
             a[i] = swap;
             i = parent(i);
         }
     }
-    public void insert(int key){
+
+    public void insert(int key) {
         heapSize = heapSize + 1;
         a[heapSize] = Integer.MIN_VALUE;
         increaseKey(heapSize, key);
     }
 
-    private int parent(int i){
+    private int parent(int i) {
         return i / 2;
     }
-    private int left(int i){
-        return (i * 2) + 1 ;
-    }
-    private int right(int i){
-        return (i * 2) + 2 ;
+
+    private int left(int i) {
+        return (i * 2) + 1;
     }
 
+    private int right(int i) {
+        return (i * 2) + 2;
+    }
 
 
 }
--- src/Test.java
+++ src/Test.java
@@ -6,10 +6,9 @@ public class Test {
 
     public static void main(String[] arg0){
         int[] b = {1, 4, 2, 3, 9, 7, 8, 10, 14, 16};
-        System.out.println(Arrays.toString(b));
+        //System.out.println(Arrays.toString(b));
         MaxHeap myHeap = new MaxHeap(b);
         System.out.println(Arrays.toString(myHeap.a));
-        System.out.println(Arrays.toString(b));
     }
 
 }
