2ff5165d904e71053cc7f8dff579d0d84393f96c
Joshua <jl6356@stu.armstrong.edu>
2017-10-27 00:17:46 -0400

    breaking a string

--- ALGORITHMS/Graph.java
+++ ALGORITHMS/Graph.java
@@ -1,2 +1,41 @@
 public class Graph {
+
+
+    int[][] adj; // adjacency matrix
+    int[] d;
+    int[] f;
+    int[] pi;
+
+    public Graph(int[][] a) {
+    }
+
+
+    // construct an graph with the adjacency matrix public Graph(int[][] adj)
+    // breadth-first search from s. results in d[], pi[]
+
+
+    // a test program
+    public static void main(String[] args) {
+        int[][] a = {{0, 0, 1, 1, 0}, {0, 0, 1, 0, 1}, {1, 1, 0, 1, 0}, {1, 0, 1, 0, 0}, {0, 1, 0, 0, 0}};
+        Graph g = new Graph(a);
+        g.bfs(3);
+        for (int i = 0; i < g.d.length; i++) {
+            System.out.print(g.d[i] + " ");
+        }
+        System.out.println();
+        g.dfs();
+        for (int i = 0; i < g.f.length; i++) {
+            System.out.print(g.d[i] + "/" + g.f[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void bfs(int s) {
+    }
+
+    // depth-first search. results in d[], f[], pi[]
+    public void dfs() {
+    }
+
+
 }
