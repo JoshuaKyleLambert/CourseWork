88e8251b3f824208461caacb275348e63065b579
Joshua <jl6356@stu.armstrong.edu>
2017-10-27 23:17:13 -0400

    Que class created

--- ALGORITHMS/Graph.java
+++ ALGORITHMS/Graph.java
@@ -1,18 +1,14 @@
 public class Graph {
 
 
-    int[][] adj; // adjacency matrix
-    int[] d;
-    int[] f;
-    int[] pi;
+    int[][] adj;    // adjacency matrix
+    int[] d;        // distance array, distance from source to vertex u
+    int[] f;        // Color attribute
+    int[] pi;       // Previous Vertex
 
     public Graph(int[][] a) {
+        adj = a;
     }
-
-
-    // construct an graph with the adjacency matrix public Graph(int[][] adj)
-    // breadth-first search from s. results in d[], pi[]
-
 
     // a test program
     public static void main(String[] args) {
@@ -30,11 +26,48 @@ public class Graph {
         System.out.println();
     }
 
+
+    // construct an graph with the adjacency matrix public Graph(int[][] adj)
+    // breadth-first search from s. results in d[], pi[]
+
     public void bfs(int s) {
     }
 
     // depth-first search. results in d[], f[], pi[]
     public void dfs() {
+    }
+
+    class Que {
+        int head = 0;
+        int tail = 0;
+        int[] que;
+
+        Que(int size) {
+            que = new int[size];
+        }
+
+        void enqueue(int s) {
+            que[tail] = s;
+
+            if (tail == que.length - 1) {
+                tail = 0;
+            } else {
+                tail++;
+            }
+
+        }
+
+        int dequeue() {
+            int x = que[head];
+            if ( head == que.length - 1 ) {
+                head = 0;
+            } else {
+                head++;
+            }
+
+            return x;
+        }
+
     }
 
 
